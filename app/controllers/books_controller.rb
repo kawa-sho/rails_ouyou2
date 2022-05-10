0d890113481666b17663290d4114888c0af1d175
EC2 Default User <ec2-user@ip-172-31-28-251.ec2.internal>
2022-05-10 12:21:58 +0000

    [Add]タグ追加機能

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -6,14 +6,45 @@ before_action :correct_user, only: [:edit, :update]
     @book = Book.find(params[:id])
     @books = Book.new
     @user = @book.user
+    @book_tags = @book.tags
     @book_comment = BookComment.new
     unless ViewCount.find_by(user_id: current_user.id, book_id: @book.id)
       current_user.view_counts.create(book_id: @book.id)
     end
+
+    #DM機能の作成
+    #自分のエントリー情報を抽出
+    @currentUserEntry = Entry.where(user_id: current_user.id)
+    #見てるページのユーザーのエントリー情報を抽出
+    @userEntry = Entry.where(user_id: @user.id)
+    #見てるページのユーザーと自分のユーザーがおんなじかどうか
+    if @user.id == current_user.id
+    else
+    #自分の持ってるエントリー情報と見てるページのユーザーのエントリー情報をすべて抽出する
+      @currentUserEntry.each do |cu|
+        @userEntry.each do |u|
+    #自分の持ってるroom_idと見てるページのユーザーの持ってるroom_idの中に同じroom_idがあるかどうか
+          if cu.room_id == u.room_id then
+    #roomはあるよと定義しroom_idをインスタンス変数で渡す
+            @isRoom = true
+            @roomId = cu.room_id
+          end
+        end
+      end
+    #roomがない場合
+      if @isRoom
+      else
+    #新しいインスタンスを制作する
+        @room = Room.new
+        @entry = Entry.new
+      end
+    end
+
   end
 
   def index
     @book = Book.new
+    @tag_list = Tag.all.sort_by {|x| x.books.count}.reverse
     #一週間のいいねの多い順
     #今日の終わり
     to = Time.current.at_end_of_day
@@ -37,7 +68,9 @@ before_action :correct_user, only: [:edit, :update]
   def create
     @book = Book.new(book_params)
     @book.user_id = current_user.id
+    tag_list=params[:book][:name].split(',')
     if @book.save
+      @book.save_tag(tag_list)
       redirect_to book_path(@book.id), notice: "You have created book successfully."
     else
       @books = Book.all
@@ -46,12 +79,15 @@ before_action :correct_user, only: [:edit, :update]
   end
 
   def edit
+    @edit = true
     @book = Book.find(params[:id])
   end
 
   def update
     @book = Book.find(params[:id])
+    tag_list=params[:book][:name].split(',')
     if @book.update(book_params)
+      @book.save_tag(tag_list)
       redirect_to book_path(@book), notice: "You have updated book successfully."
     else
       render "edit"
@@ -62,6 +98,15 @@ before_action :correct_user, only: [:edit, :update]
     @book = Book.find(params[:id])
     @book.destroy
     redirect_to books_path
+  end
+
+  def search_tag
+  #検索結果画面でもタグ一覧表示
+    @tag_list=Tag.all
+　#検索されたタグを受け取る
+    @tag=Tag.find(params[:tag_id])
+　#検索されたタグに紐づく投稿を表示
+    @posts=@tag.posts.page(params[:page]).per(10)
   end
 
   private
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_05_08_121914) do
+ActiveRecord::Schema.define(version: 2022_05_10_085039) do
 
   create_table "active_storage_attachments", force: :cascade do |t|
     t.string "name", null: false
@@ -46,6 +46,16 @@ ActiveRecord::Schema.define(version: 2022_05_08_121914) do
     t.integer "book_id"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "book_tags", force: :cascade do |t|
+    t.integer "book_id"
+    t.integer "tag_id"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["book_id", "tag_id"], name: "index_book_tags_on_book_id_and_tag_id", unique: true
+    t.index ["book_id"], name: "index_book_tags_on_book_id"
+    t.index ["tag_id"], name: "index_book_tags_on_tag_id"
   end
 
   create_table "books", force: :cascade do |t|
@@ -113,6 +123,12 @@ ActiveRecord::Schema.define(version: 2022_05_08_121914) do
     t.datetime "updated_at", precision: 6, null: false
   end
 
+  create_table "tags", force: :cascade do |t|
+    t.string "name", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
@@ -136,6 +152,8 @@ ActiveRecord::Schema.define(version: 2022_05_08_121914) do
 
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
   add_foreign_key "active_storage_variant_records", "active_storage_blobs", column: "blob_id"
+  add_foreign_key "book_tags", "books"
+  add_foreign_key "book_tags", "tags"
   add_foreign_key "entries", "rooms"
   add_foreign_key "entries", "users"
   add_foreign_key "group_users", "groups"
